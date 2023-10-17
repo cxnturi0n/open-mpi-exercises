5fa7185bb9a76a2b38b8788b44fc8d0d5a67e959
antonio-petrillo <antonio.petrillo4@studenti.unina.it>
2023-10-17 09:55:47 +0200

    Add control on input less than 0

--- sum-n-numbers/choice_strategy/constants.h
+++ sum-n-numbers/choice_strategy/constants.h
@@ -5,6 +5,7 @@ typedef enum { TAG_MERGE, TAG_DISTRIBUTE } tag_t;
 typedef enum { STRATEGY_1, STRATEGY_2, STRATEGY_3, STRATEGY_WRONG } strategy_t;
 
 #define WRONG_ARG_MESSAGE "Usage: ./sum_n_numbers n strategy"
+#define INVALID_INPUT_NUMBER "[ERROR]: input 'n' should not be negative"
 #define NOT_A_NUMBER_ERROR_MESSAGE "[ERROR]: given input is not a number\n"
 #define STRATEGY_WRONG_MESSAGE "[ERROR]: wrong strategy, choose between 1, 2, 3"
 
