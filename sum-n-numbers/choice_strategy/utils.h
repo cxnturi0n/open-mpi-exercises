1e5d2bf8472bb37c640d9c9edde18cceeb7f1859
cxnturi0n <f.cinicolo@studenti.unina.it>
2023-10-20 12:35:26 +0200

    added missing strategy_to_str impl

--- sum-n-numbers/choice_strategy/utils.h
+++ sum-n-numbers/choice_strategy/utils.h
@@ -13,5 +13,6 @@ int is_number(char *input);
 double random_number();
 void check_if_strategy_is_applicable(strategy_t *strategy, int nprocs);
 strategy_t parse_strategy(char *input);
+char* strategy_to_str(strategy_t selected_strategy);
 
 #endif // UTILS_H_
