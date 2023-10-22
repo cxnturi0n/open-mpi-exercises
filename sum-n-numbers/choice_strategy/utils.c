ee80bd9c477e199470cde7d7ff0aa1b183d02b47
antonio-petrillo <antonio.petrillo4@studenti.unina.it>
2023-10-22 10:03:08 +0200

    modify pbs to autorun n-times each execution

--- sum-n-numbers/choice_strategy/utils.c
+++ sum-n-numbers/choice_strategy/utils.c
@@ -1,18 +1,14 @@
 #include "utils.h"
 
-int is_number(char *input)
-{
+int is_number(char *input) {
   int i = 0;
   char c;
-  if (!strlen(input))
-  {
+  if (!strlen(input)) {
     return 0;
   }
-  while (input[i] != '\0')
-  {
+  while (input[i] != '\0') {
     c = input[i];
-    if (!isdigit(c))
-    {
+    if (!isdigit(c)) {
       return 0;
     }
     i++;
@@ -20,53 +16,41 @@ int is_number(char *input)
   return 1;
 }
 
-double random_number()
-{
+double random_number() {
   /* return 1; */
   /* return rand() % RAND_MAX_RANGE; */
   return (double)rand() / RAND_MAX;
 }
 
-void check_if_strategy_is_applicable(strategy_t *strategy, int nprocs)
-{
+void check_if_strategy_is_applicable(strategy_t *strategy, int nprocs) {
   // redirect to strategy 1 if
   // - the number of processors is not a power of 2
   // - and
   // - the selected strategy is 2 or 3
   if ((*strategy == STRATEGY_2 || *strategy == STRATEGY_3) &&
-      !is_power_of_2(nprocs))
-  {
+      !is_power_of_2(nprocs)) {
     *strategy = STRATEGY_1;
   }
 }
 
-strategy_t parse_strategy(char *input)
-{
+strategy_t parse_strategy(char *input) {
   int len = strlen(input);
-  if (!len || len > 1)
-  {
+  if (!len || len > 1) {
     return STRATEGY_WRONG;
   }
   strategy_t selected = STRATEGY_WRONG;
-  if (input[0] == '1')
-  {
+  if (input[0] == '1') {
     selected = STRATEGY_1;
-  }
-  else if (input[0] == '2')
-  {
+  } else if (input[0] == '2') {
     selected = STRATEGY_2;
-  }
-  else if (input[0] == '3')
-  {
+  } else if (input[0] == '3') {
     selected = STRATEGY_3;
   }
   return selected;
 }
 
-char *strategy_to_str(strategy_t selected_strategy)
-{
-  switch (selected_strategy)
-  {
+char *strategy_to_str(strategy_t selected_strategy) {
+  switch (selected_strategy) {
   case STRATEGY_1:
     return "Strategy 1";
   case STRATEGY_2:
