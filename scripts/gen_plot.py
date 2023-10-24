2af3636830adf48a4b82ab588f515689340ba4b4
cxnturi0n <f.cinicolo@studenti.unina.it>
2023-10-24 17:30:31 +0200

    Addedd graphs, modified gen_ploy.py script

--- scripts/gen_plot.py
+++ scripts/gen_plot.py
@@ -1,36 +1,50 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import csv
 import sys
 
-filename = sys.argv[1]
-time_index = 5 if len(sys.argv) == 2 else 4
-plot_name = "plot " + filename[6:9]
+if len(sys.argv) < 4:
+    print("Usage: python script.py <input_path> <y_label_name> <output_path> <plot_name>")
+    sys.exit(1)
 
-y_label = "time " + ("max" if time_index == 5 else "avg")
+input_path = sys.argv[1]
+y_label = sys.argv[2]
+output_path = sys.argv[3]
+plot_name = sys.argv[4]
 
-nprocs = [1,2,4,8]
-time_max_strategy_I = []
-time_max_strategy_II = []
-time_max_strategy_III = []
+nprocs = [1, 2, 4, 8]
 
-with open(filename, 'r') as csvfile:
-    lines = csv.reader(csvfile, delimiter = ',')
+time_strategy_I = []
+time_strategy_II = []
+time_strategy_III = []
+
+with open(input_path, 'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    next(lines) 
     for row in lines:
-        if row[2] == 'Strategy 1':
-            time_max_strategy_I.append(float(row[4]))
-        if row[2] == 'Strategy 2':
-            time_max_strategy_II.append(float(row[4]))
-        if row[2] == 'Strategy 3':
-            time_max_strategy_III.append(float(row[4]))
+        if  y_label == 'avg_time':
+            if row[2] == 'Strategy 1':
+                time_strategy_I.append(float(row[4]))
+            if row[2] == 'Strategy 2':
+                time_strategy_II.append(float(row[4]))
+            if row[2] == 'Strategy 3':
+                time_strategy_III.append(float(row[4]))
+        else :
+            if row[2] == 'Strategy 1':
+                time_strategy_I.append(float(row[5]))
+            if row[2] == 'Strategy 2':
+                time_strategy_II.append(float(row[5]))
+            if row[2] == 'Strategy 3':
+                time_strategy_III.append(float(row[5]))
+                   
 
-plt.plot(nprocs, time_max_strategy_I, color = 'g', linestyle='dashed', marker='o', label="strategy I")
-plt.plot(nprocs, time_max_strategy_II, color = 'b', linestyle='dashed', marker='o', label="strategy II")
-plt.plot(nprocs, time_max_strategy_III, color = 'r', linestyle='dashed', marker='o', label="strategy III")
+plt.plot(nprocs, time_strategy_I, color='g', linestyle='dashed', marker='o', label="strategy I")
+plt.plot(nprocs, time_strategy_II, color='b', linestyle='dashed', marker='o', label="strategy II")
+plt.plot(nprocs, time_strategy_III, color='r', linestyle='dashed', marker='o', label="strategy III")
 plt.xlabel('nprocs')
 plt.ylabel(y_label)
-plt.title(plot_name, fontsize = 20)
+plt.title(plot_name, fontsize=20)
 
 plt.grid()
 plt.legend()
-plt.show()
+image_path = f"{output_path}/{plot_name}.png"
+plt.savefig(image_path)  
