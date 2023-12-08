2bc268a0de0774dcb1a03de4e27244b8f4f1e801
antonio-petrillo <antonio.petrillo4@studenti.unina.it>
2023-12-08 19:20:13 +0100

    fix typo

--- mat-vet-openmp/src/main.c
+++ mat-vet-openmp/src/main.c
@@ -153,7 +153,7 @@ double *matxvet_strat(int row_size, int col_size, double *vector,
   }
 
 #pragma omp parallel for default(none)                                         \
-    shared(n, m, matrix, vector, y) private(i, j)
+    shared(row_size, col_size, matrix, vector, y) private(i, j)
   for (i = 0; i < row_size; i++) {
     for (j = 0; j < col_size; j++) {
       result[i] += matrix[i][j] * vector[j];
